07a80c76067ac1fe6d7c86344caf5eac99cc9222
DESKTOP-SSEISDQ\Tech Usman Arshad <usmanaryk1@gmail.com>
2022-02-18 14:46:29 +0500

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,14 @@ import { Store } from './store';
 })
 export class AppComponent {
   title = 'stateManagementNgrxStore';
-
+  //2- and here we ask for the data
   todos$ = this.store.select<any[]>('todos');
   
   constructor(private store:Store){
     // console.log(this.store);
+    //1-here we are populating the initializing date
     this.store.set('todos', [{id:1, name:'usman'},{id:2, name:'adnan'}])
-    // this.todos$.subscribe(ok=> console.log(ok))
+    this.todos$.subscribe(ok=> console.log(ok))
   }
   
 }
